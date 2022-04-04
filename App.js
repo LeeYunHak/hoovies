48c480e4814937ad48867e3207babbe0ca27b3a0
LeeYunHak <sza0203@naver.com>
2022-04-04 16:57:52 +0900

    useAssets & useFonts Hooks

--- App.js
+++ App.js
@@ -3,25 +3,13 @@ import AppLoading from "expo-app-loading";
 import { Text, Image } from "react-native";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    // await 해야하는 모든 로딩을 담당하는 곳
-    await Font.loadAsync(Ionicons.font); // Ionicons의 font 파일에 접근
-    await Asset.loadAsync(require("./test.jpeg")); // 로컬에서 가져오기
-    await Image.prefetch("https://reactnative.dev/img/oss_logo.png"); // 웹에서 가져오기
-  };
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  const [assets] = useAssets([require("./test.jpeg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
   return <Text>We are done loading</Text>;
 }
