2667b1def1975c650d6ad794a54cd5b8cf3f02e3
LeeYunHak <sza0203@naver.com>
2022-04-04 15:54:30 +0900

    AppLoading Components Part Two Asset & Font

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { useState } from "react";
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
+
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
     // await 해야하는 모든 로딩을 담당하는 곳
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    await Font.loadAsync(Ionicons.font); // Ionicons의 font 파일에 접근
+    await Asset.loadAsync(require("./test.jpeg")); // 로컬에서 가져오기
+    await Image.prefetch("https://reactnative.dev/img/oss_logo.png"); // 웹에서 가져오기
   };
   if (!ready) {
     return (
