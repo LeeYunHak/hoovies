8fa486d65b31d8f72ccd514a512e8b3a443e634b
LeeYunHak <sza0203@naver.com>
2022-04-08 03:06:28 +0900

    #2.15 Themes

--- App.js
+++ App.js
@@ -7,16 +7,22 @@ import { Asset, useAssets } from "expo-asset";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 import Root from "./navigation/Root";
+import { useColorScheme } from "react-native";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./seyled";
 
 export default function App() {
   const [assets] = useAssets([require("./test.jpeg")]);
   const [loaded] = Font.useFonts(Ionicons.font);
+  const isDark = useColorScheme() === "dark";
   if (!assets || !loaded) {
     return <AppLoading />;
   }
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
